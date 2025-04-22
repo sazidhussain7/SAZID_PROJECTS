4c295b0701daa2c592c6157a8548018a6ea31e90
sazidhussain7 <sazid7hussain@gmail.com>
2025-04-22 14:01:00 +0530

    This is my first project.

---
+++ sazid_portfolio.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    document.getElementById("demo1").innerHTML="Congratulations !";
+    document.getElementById("demo2").innerHTML="You hired a very honest person.";    
+
+};
+function project(){
+    document.getElementById("demo3").innerHTML="These are my project !";
+    document.getElementById("demo4").innerHTML="These are my project !";
+    document.getElementById("demo5").innerHTML="These are my project !";
+    document.getElementById("demo6").innerHTML="These are my project !";
+
+}
